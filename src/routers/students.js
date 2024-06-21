769f5e41dc402fbd67cc8a92ec0647e6351b9bee
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2024-06-21 12:38:19 +0300

    ctrlWrapperUtil

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,39 +1,14 @@
 import { Router } from 'express';
-import { getAllStudents, getStudentById } from './services/students.js';
+import {
+  getAllStudentsController,
+  getStudentByIdController,
+} from '../controllers/students.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const studentsRouter = Router();
 
-studentsRouter.get('/', async (req, res) => {
-  const students = await getAllStudents();
+studentsRouter.get('/', ctrlWrapper(getAllStudentsController));
 
-  res.status(200).json({
-    data: students,
-  });
-});
-
-studentsRouter.get('/:studentId', async (req, res) => {
-  try {
-    const { studentId } = req.params;
-    const student = await getStudentById(studentId);
-
-    if (!student) {
-      return res.status(404).json({
-        message: 'Student not found',
-      });
-    }
-
-    res.status(200).json({
-      data: student,
-    });
-  } catch (error) {
-    if (error.message.includes('Cast to ObjectId failed')) {
-      error.status = 404;
-    }
-    const { status = 500 } = error;
-    res.status(status).json({
-      message: error.message,
-    });
-  }
-});
+studentsRouter.get('/:studentId', ctrlWrapper(getStudentByIdController));
 
 export default studentsRouter;

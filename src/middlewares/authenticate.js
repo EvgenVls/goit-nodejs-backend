6696dc5a79af5d052beb5fb8d08adb6981dfa269
EvgenVls <145988014+EvgenVls@users.noreply.github.com>
2024-07-11 18:21:25 +0300

    update authenticate

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -32,7 +32,7 @@ const authenticate = async (req, res, next) => {
     new Date() > new Date(session.accessTokenValidUntil);
 
   if (isAccessTokenExpired) {
-    next(createHttpError(401, 'Access token expired'));
+    return next(createHttpError(401, 'Access token expired'));
   }
 
   const user = UsersCollection.findById(session.userId);
